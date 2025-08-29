57e58e42b7b728a93d087f1e43b7a5ba744c3024
kartikparhad04 <kartikparhad04@gmail.com>
2025-08-30 01:37:59 +0530

    If elseIf Programe

---
+++ Core/src/Demoprogram/Age.java
@@ -0,0 +1,31 @@
+package Demoprogram;
+
+public class Age {
+
+	public static void main(String[] args) {
+		
+		int age = -1;
+		
+		if(age>0 && age<=10)
+		{
+			System.out.println("Child");
+			
+		}
+         
+		else if(age>10 && age<=20)
+		{
+			System.out.println("Teenager");
+		}
+		
+		else if(age>20 && age<40)
+		{
+			System.out.println("Young");
+		}
+		
+		else
+		{
+			System.out.println("Old");
+		}
+	}
+
+}
